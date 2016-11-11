fc8ef3171369d3c65d1f5813bc595b204aadd8ea
onlyzs89 <onlyzs1023@gmail.com>
2016-11-11 14:20:33 +0900

    Create import_earthquake.py

---
+++ import_earthquake.py
@@ -0,0 +1,75 @@
+# -*- coding: utf-8 -*-
+import sys
+import json
+from elasticsearch import Elasticsearch
+from datetime import datetime
+import requests
+from threading import Timer
+
+datestring = "2016/06/01 07:37:19.621" #start from this datetime
+
+class GetEarthQuake():
+	def __init__(self):
+		self.__es = Elasticsearch()
+		
+	def run(self):
+		Timer(60, self.run).start() #interval time: 1 minute
+		
+		r = requests.get('http://api.p2pquake.com/v1/human-readable') #get json data
+		jsonData = r.json()
+		
+		global datestring		
+		new_date=datestring #save the latest time
+		print datestring
+		
+		for value in jsonData:
+			if value["code"]==551: 
+				if value["time"]>datestring:
+					if value["time"]>new_date:
+						new_date=value["time"]
+					
+					##process data previously
+					#location:
+					lat=value["earthquake"]["hypocenter"]["latitude"]
+					lon=value["earthquake"]["hypocenter"]["longitude"]
+					if lat.startswith("N"):
+						lats=float(lat.lstrip("N"))
+					else:
+						lats=float(lat.lstrip("S"))
+					if lon.startswith("E"):
+						lons=float(lon.lstrip("E"))
+					else:
+						lons=float(lon.lstrip("W"))
+					
+					#depth
+					dep=value["earthquake"]["hypocenter"]["depth"]
+					if dep.endswith("km"):
+						depth=float(dep.rstrip("km"))
+					else:
+						depth=0
+						
+					#datetime
+					dtime=value["earthquake"]["time"].encode("utf-8")
+					dtime=datetime.strptime(dtime,"%d日%H時%M分")
+					dtime=dtime.replace(year=datetime.today().year)
+					dtime=dtime.replace(month=datetime.today().month)
+					dtime=datetime.strftime(dtime,"%Y/%m/%d %H:%M:%S")
+					dtime=dtime+"+09:00" #add timezone
+					
+					doc = {
+						"time":dtime,
+						"place":value["earthquake"]["hypocenter"]["name"],
+						"location":{
+							"lat":lats,
+							"lon":lons
+						},
+						"magnitude":value["earthquake"]["hypocenter"]["magnitude"],
+						"depth":depth
+					}
+					self.__es.index(index="earthquakes", doc_type='earthquake', body=doc)
+					
+		datestring=new_date #next time from the latest time
+		
+
+if __name__ == '__main__':
+	GetEarthQuake().run()
